5d19616e0586fc774bc80da415adb0e8da8a742b
Its_Eagle <127813456+LofiGodam@users.noreply.github.com>
2023-05-29 14:23:23 +0530

    Update __init__.py
    Signed-off-by: Its_Eagle  <127813456+LofiGodam@users.noreply.github.com>

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -7,7 +7,7 @@ import telegram.ext as tg
 from pyrogram import Client, errors
 from telethon import TelegramClient
 from telethon import TelegramClient
-from FallenRobot.quotstuff.quotapi import quotly
+from FallenRobot.quotstuff.quotapi import Quotly
 
 StartTime = time.time()
 
