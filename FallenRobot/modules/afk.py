ee0c79eea7410cc465a75476c0d23c5c1a203726
Its_Eagle <127813456+LofiGodam@users.noreply.github.com>
2023-07-13 08:17:59 +0530

    Update afk.py
    Signed-off-by: Its_Eagle  <127813456+LofiGodam@users.noreply.github.com>

--- FallenRobot/modules/afk.py
+++ FallenRobot/modules/afk.py
@@ -159,7 +159,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "💤Aꜰᴋ💤​"
+__mod_name__ = "Aꜰᴋ​"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
