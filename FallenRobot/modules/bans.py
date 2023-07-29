6b8e6f8739c2ca83178aba0c9de039ca60f11dff
Its_Eagle <127813456+LofiGodam@users.noreply.github.com>
2023-07-29 12:39:39 +0530

    Update bans.py
    Signed-off-by: Its_Eagle  <127813456+LofiGodam@users.noreply.github.com>

--- FallenRobot/modules/bans.py
+++ FallenRobot/modules/bans.py
@@ -425,7 +425,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(KICKME_HANDLER)
 
-__mod_name__ = "👹Bᴀɴs​👹"
+__mod_name__ = "Bᴀɴs​"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
