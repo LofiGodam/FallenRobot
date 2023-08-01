fe19e7b6f9af40ac8d317fc9b9e5206887ef8b3d
Its_Eagle <127813456+LofiGodam@users.noreply.github.com>
2023-08-01 08:56:26 +0530

    Update __main__.py
    Signed-off-by: Its_Eagle  <127813456+LofiGodam@users.noreply.github.com>

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -204,7 +204,7 @@ def start(update: Update, context: CallbackContext):
                 "CAACAgUAAxkBAAIUnWRw7NDuUHC2RuPhmNEHN-XlLGKLAAISDQACDLSJVzV0gUfJ8ariLwQ" 
             )
             update.effective_message.reply_text(
-                PM_START_IMG.format(escape_markdown(first_name), START_IMG),
+                PM_START_IMG.format(escape_markdown(first_name), (START_IMG), BOT_NAME),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
