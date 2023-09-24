a422d72d732c1a9dc9cb809de77a1df076a53596
Its_Eagle <127813456+LofiGodam@users.noreply.github.com>
2023-09-24 19:47:21 +0530

    Update __main__.py
    Signed-off-by: Its_Eagle  <127813456+LofiGodam@users.noreply.github.com>

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -104,7 +104,7 @@ buttons = [
        InlineKeyboardButton(text="♡ 𝗦𝗨𝗣𝗣𝗢𝗥𝗧 ♡", url=f"https://t.me/{SUPPORT_CHAT}"),
     ],
     [
-       InlineKeyboardButton(text="♡ 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 ♡", url=f"https://t.me/DragonaboutDpz"),
+       InlineKeyboardButton(text="♡ 𝗥𝗘𝗣𝗢 ♡", url=f"https://te.legra.ph/file/f9d917ef57f9376d8648e.mp4", callback_data="repo_back"),
     ],
 ]
 HELP_STRINGS = f"""
